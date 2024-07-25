3277149a402f3b55a3eb19563234c0b979fe6628
Naveen Kumar <n999.9k1986@gmail.com>
2024-07-25 14:44:14 +0530

    Add project structure

---
+++ src/main/java/com/motg/server/MotgPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.motg.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotgPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotgPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motg/server/MotgPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motg.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotgPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
